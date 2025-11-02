cd05ca752e7dd2b44e5ff807c7192d587b220f95
Rahul Ranjan <rahulranjan1245@gmail.com>
2025-11-02 20:34:09 +0530

    Time: 4 ms (5.96%) | Memory: 44.6 MB (6.30%) - LeetSync

---
+++ MySql/3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum =nums[0];
+        for(int i =1 ;i<nums.length; i++){
+            if(nums[i-1]+1 != nums[i]){
+                break;
+            }
+                sum+= nums[i];
+        }
+
+        Arrays.sort(nums);
+       
+        int i =0;
+       while(i<nums.length){
+        if(sum == nums[i]){
+            sum++;
+
+        }
+        i++;
+
+       }
+
+        return sum;
+    }
+}
